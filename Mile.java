44a489fceed9bedf6a30eeb5d9d6d1f95cf27da0
Елена Сташевская <e.stashevskaya@spider.int>
2020-08-21 13:02:33 +0300

    fixed a bug

--- Mile.java
+++ Mile.java
@@ -1,8 +1,7 @@
 public class Mile {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        double cost = 75.00;
-        int mile = service.calculate(cost);
+        int mile = service.calculate(75);
         System.out.println(mile);
     }
 }
