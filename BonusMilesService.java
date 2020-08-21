7fa1362f17e7fb06c4fcd7e99f9de56d7f3c4f7f
Елена Сташевская <e.stashevskaya@spider.int>
2020-08-21 12:59:27 +0300

    change project

---
+++ BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate (double cost) {
+        int mile = (int)cost/20;
+        return  mile;
+    }
+}
--- Mile.java
+++ Mile.java
@@ -1,7 +1,8 @@
 public class Mile {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
         double cost = 75.00;
-        int mile = (int)cost/20;
+        int mile = service.calculate(cost);
         System.out.println(mile);
     }
 }
